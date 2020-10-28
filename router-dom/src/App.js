2329fe928038f75f953666040c01750804316b5a
Park <arrans9591@gmail.com>
2020-10-28 21:02:50 +0900

    used react-router-dom

--- router-dom/src/App.js
+++ router-dom/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Rooms from './Rooms';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App(){
+  return(
+    <BrowserRouter>
+      <div style={{padding : 20, border : '5px solid gray'}}>
+        <Link to ="/">홈</Link>
+        <br />
+        <Link to ="/photo">사진</Link>
+        <br />
+        <Link to ="/rooms">방 소개</Link>
+        <br />
+        <Route exact path="/" component={Home} />
+        <Route path="/photo" component={Photo} />
+        <Route path="/rooms" component={Rooms} />
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
+function Home ({match}){
+  return <h2> 이곳은 홈페이지 입니다.</h2>
+}
+
+function Photo ({match}){
+  return <h2> 여기서 사진을 감상 하세요 </h2>
+}
